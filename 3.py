8f494deba75a2eb915d655fc0acf14b850b1103a
faresovv <65291575+faresovv@users.noreply.github.com>
2020-05-21 20:24:39 +0400

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+data = np.loadtxt("laba3.txt")
+plt.plot(data[:, 0], data[:, 1])
+plt.plot([0,1,2,3,4,5],[10.8,
+8.5,
+8.4,
+13.7,
+8.9,
+12.7
+],'ro')
+plt.show()
