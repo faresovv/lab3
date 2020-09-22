19a574f6b2f8dca529d9dea3778f483db7c2d838
faresovv <65291575+faresovv@users.noreply.github.com>
2020-09-22 23:46:37 +0300

    Add files via upload

---
+++ 2 (1).py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+data = np.loadtxt("graf.txt")
+plt.plot(data[:, 0], data[:, 1])
+plt.plot([1818,
+1819,
+1820,
+1821,
+1822,
+1823,
+1824,
+1825,
+1826,
+1827,
+1828,
+1829],[12,
+13.6,
+13.4,
+12.8,
+13.9,
+14.4,
+11.8,
+15.1,
+15.7,
+14.5,
+15.1,
+13],'ro')
+plt.show()
---
+++ 4 (1).py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+data = np.loadtxt("laba32.txt")
+plt.plot(data[:, 0], data[:, 1])
+plt.plot([1898,1899,1900,1901,1902,1903],[10.8,
+8.5,
+8.4,
+13.7,
+8.9,
+12.7
+],'ro')
+plt.show()
